2ce8603efdccd722f1dab849715f3f5ae05c8e4a
Rasmus <scratty@gmail.com>
2024-01-20 13:30:19 +0100

    First working commit. This gives me a static library a-file but in the developer temp dir.

---
+++ AudioSourceHandler.cpp
@@ -0,0 +1,14 @@
+//
+//  AudioSourceHandler.cpp
+//  8-Bit Audio Emulator Lib
+//
+//  Created by Rasmus Anthin on 2024-01-20.
+//
+
+#include "AudioSourceHandler.h"
+#include <stdio.h>
+
+void AudioSourceHandler::print() const
+{
+  printf("Hello World\n");
+}
---
+++ AudioSourceHandler.h
@@ -0,0 +1,18 @@
+//
+//  AudioSourceHandler.h
+//  8-Bit Audio Emulator Lib
+//
+//  Created by Rasmus Anthin on 2024-01-19.
+//
+
+#pragma once
+
+#include <AL/al.h>
+#include <AL/alc.h>
+
+class AudioSourceHandler
+{
+  AudioSourceHandler();
+  
+  void print() const;
+};
