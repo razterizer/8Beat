f2a6c80e0fcbdd392da170191a82e3f003b566cb
Rasmus <scratty@gmail.com>
2024-01-22 01:40:25 +0100

    Adding a simple copy script that is supposed to copy the corresponding lib files built by the project from its DerivedData folder to the project folder as doing such things in a post-build script is a pain in the butt in xcode/macos.

---
+++ copy_lib.sh
@@ -0,0 +1,4 @@
+cp /Users/rasmusanthin/Library/Developer/Xcode/DerivedData/8-Bit_Audio_Emulator_Lib-epvojosajpcbxffuouyzozzjarau/Build/Products/Debug/lib8-Bit\ Audio\ Emulator\ Lib.a ./build/Debug/
+cp /Users/rasmusanthin/Library/Developer/Xcode/DerivedData/8-Bit_Audio_Emulator_Lib-epvojosajpcbxffuouyzozzjarau/Build/Products/Release/lib8-Bit\ Audio\ Emulator\ Lib.a ./build/Release/
+chmod ugo+rwx ./build/Release/*.a
+chmod ugo+rwx ./build/Debug/*.a
