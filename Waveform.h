408054d9583c99a4a13081d7bff9de909e5395bb
Rasmus <scratty@gmail.com>
2024-02-22 16:47:43 +0100

    Waveform: * Adding a default constructor and a custom constructor in Waveform that takes arguments N and dc_val and that fills the buffer with N elements of value dc_val. * Adding function copy_properties() that takes another Waveform object as an argument and copies the member variables frequency, sample_rate and duration from the object argument. * Adding function update_duration() that updates the duration member variable from the sample_rate and buffer size. Quite useful function and perhaps more useful than the counterpart found in WaveformHelper.

--- Waveform.h
+++ Waveform.h
@@ -14,10 +14,30 @@ namespace audio
   // The returned data from the waveform generator or file loader.
   struct Waveform
   {
+    Waveform() = default;
+    // Initialize the buffer with DC value.
+    Waveform(size_t N, float dc_val)
+     : buffer(N, dc_val)
+    {}
+  
     std::vector<float> buffer;
     float frequency = 440.f; // A4
     float sample_rate = 44100.f;
     float duration = 5.f;
+    
+    void copy_properties(const Waveform& wave)
+    {
+      this->frequency = wave.frequency;
+      this->sample_rate = wave.sample_rate;
+      this->duration = wave.duration;
+    }
+    
+    void update_duration()
+    {
+      float dt = 1/sample_rate;
+      size_t num_samples = buffer.size();
+      this->duration = num_samples * dt;
+    }
   };
 
 }
