7596bbfb3156e1ae6567807d254c2150a0fbdd55
Rasmus <scratty@gmail.com>
2024-02-22 16:49:17 +0100

    Spectrum: * Just as for the struct Waveform, we add a function copy_properties() here as well which does things similarly as in the Waveform counterpart.

--- Spectrum.h
+++ Spectrum.h
@@ -19,6 +19,12 @@ namespace audio
     std::vector<std::complex<float>> buffer;
     float freq_start = 0.f;
     float freq_end = 0.f;
+    
+    void copy_properties(const Spectrum& spectrum)
+    {
+      this->freq_start = spectrum.freq_start;
+      this->freq_end = spectrum.freq_end;
+    }
   };
 
 }
