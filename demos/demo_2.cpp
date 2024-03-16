82b981cf0d72e6dea6432e127f1d19c797853767
Rasmus Anthin <scratty@gmail.com>
2024-03-16 19:57:34 +0100

    Adding demo_2.cpp + sample wav files that I made sure are free to use and should be ok to use here in this context. I also added an attribution.txt file to make sure the creators get their due credit. Added some libsnd and openal_soft settings in the project settings, just in case.

---
+++ demos/demo_2.cpp
@@ -0,0 +1,66 @@
+//
+//  demo_2.cpp
+//  8-Bit Audio Emulator Lib
+//
+//  Created by Rasmus Anthin on 2024-03-16.
+//
+
+#include "../../../lib/8-Bit Audio Emulator Lib/AudioSourceHandler.h"
+#include "../../../lib/8-Bit Audio Emulator Lib/WaveformGeneration.h"
+#include "../../../lib/8-Bit Audio Emulator Lib/WaveformHelper.h"
+#include "../../../lib/8-Bit Audio Emulator Lib/WaveformIO.h"
+#include <filesystem>
+
+
+int main(int argc, char** argv)
+{
+  audio::AudioSourceHandler src_handler;
+  audio::WaveformGeneration wave_gen;
+  
+  std::string wk_dir = "./sounds/";
+  std::cout << std::filesystem::current_path().string() << std::endl;
+  auto wd_cat_ld = audio::WaveformIO::load(wk_dir + "Cat_Meow_2-Cat_Stevens-2034822903.wav", 3);
+  if (wd_cat_ld.has_value())
+  {
+    const auto& wd_cat = wd_cat_ld.value();
+    auto src_cat = src_handler.create_source_from_waveform(wd_cat);
+    std::cout << "Cat (samples): " << wd_cat.buffer.size() << std::endl;
+    src_cat->play(audio::PlaybackMode::STATE_WAIT);
+
+    std::cout << "---\n";
+    audio::WaveformIO::save(wd_cat, wk_dir + "cat meow saved.wav", audio::AudioFileFormatSubType::PCM_16, 3);
+    
+    // Reverb
+    
+    bool load_and_play_reverb = false; // If false, creates reverb and saves it to file.
+    
+    if (!load_and_play_reverb)
+    {
+      std::cout << "---\n";
+      auto wd_rev_ld = audio::WaveformIO::load(wk_dir + "07a-AirRaidShelterCentre-DPA 4061-Stereo-48K-BURST.wav", 3);
+      if (wd_rev_ld.has_value())
+      {
+        auto& wd_rev = wd_rev_ld.value();
+        auto wd_cat_in_air_raid_shelter = audio::WaveformHelper::reverb_fast(wd_cat, wd_rev);
+        auto src_cat_in_air_raid_shelter = src_handler.create_source_from_waveform(wd_cat_in_air_raid_shelter);
+        std::cout << "Cat in Air Raid Shelter (samples): " << wd_cat_in_air_raid_shelter.buffer.size() << std::endl;
+        src_cat_in_air_raid_shelter->play(audio::PlaybackMode::STATE_WAIT);
+        std::cout << "---\n";
+        audio::WaveformIO::save(wd_cat_in_air_raid_shelter, wk_dir + "cat meow reverb saved.wav", audio::AudioFileFormatSubType::PCM_16, 3);
+      }
+    }
+    else
+    {
+      std::cout << "---\n";
+      auto wd_cat_in_air_raid_shelter_ld = audio::WaveformIO::load(wk_dir + "cat meow reverb saved.wav", 3);
+      if (wd_cat_in_air_raid_shelter_ld.has_value())
+      {
+        const auto& wd_cat_in_air_raid_shelter = wd_cat_in_air_raid_shelter_ld.value();
+        auto src_cat_in_air_raid_shelter = src_handler.create_source_from_waveform(wd_cat_in_air_raid_shelter);
+        src_cat_in_air_raid_shelter->play(audio::PlaybackMode::STATE_WAIT);
+      }
+    }
+  }
+ 
+  return 0;
+}
