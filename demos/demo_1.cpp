e728ec1aac8d63a574e5f9162ed8b154cb46b8fe
Rasmus Anthin <scratty@gmail.com>
2024-03-16 11:33:47 +0100

    Adding first demo demo_1. Only supports linmac at this stage. TODO: Add vcxproj for demos.

---
+++ demos/demo_1.cpp
@@ -0,0 +1,111 @@
+//
+//  demo_1.cpp
+//  8-Bit Audio Emulator Lib
+//
+//  Created by Rasmus Anthin on 2024-03-16.
+//
+
+#include "../../../lib/8-Bit Audio Emulator Lib/AudioSourceHandler.h"
+#include "../../../lib/8-Bit Audio Emulator Lib/WaveformGeneration.h"
+#include "../../../lib/8-Bit Audio Emulator Lib/WaveformHelper.h"
+
+
+int main(int argc, char** argv)
+{
+  audio::AudioSourceHandler src_handler;
+  audio::WaveformGeneration wave_gen;
+  
+  auto wave_func = [](float phi, float /*param*/) -> float
+  {
+    return std::sin(0.5f*std::sin(phi)*std::sqrt(phi));
+  };
+  
+  auto freq_func = [](float t, float duration, float freq_0) -> float
+  {
+    float hd = duration * 0.5f;
+    if (0.f <= t && t < hd)
+      return math::linmap(t, 0.f, hd, 800.f, freq_0);
+    else if(hd <= t && t <= duration)
+      return math::linmap(t, hd, duration, freq_0, 800.f);
+    else
+      return freq_0;
+  };
+  
+  auto ampl_func = [](float t, float duration)
+  {
+    float hd = duration * 0.5f;
+    if (0.f <= t && t < hd)
+      return math::linmap(t, 0.f, hd, 0.1f, 1.f);
+    else if(hd <= t && t <= duration)
+      return math::linmap(t, hd, duration, 1.f, 0.f);
+    else
+      return 1.f;
+  };
+  
+  // //////////////////////
+  
+  enum class TestType { TEST_SIMPLE, TEST_WAVE_LAMBDA, TEST_FREQ_LAMBDA, TEST_AMPL_LAMBDA, TEST_ALL_LAMBDA, TEST_ALL_ENUM };
+  TestType test = TestType::TEST_SIMPLE;
+  
+  auto duration_s = 2.f;
+  
+  auto freq = 440.f;
+  
+  audio::Waveform wd;
+  
+#if 1
+  float sample_rate = 44'100;
+#else
+  float sample_rate = 10'000;
+#endif
+
+  switch (test)
+  {
+    case TestType::TEST_SIMPLE:
+      wd = wave_gen.generate_waveform(audio::WaveformType::SINE_WAVE, duration_s, freq,
+        audio::FrequencyType::CONSTANT, audio::AmplitudeType::CONSTANT, audio::PhaseType::ZERO, 44100);
+      break;
+    case TestType::TEST_WAVE_LAMBDA:
+#if 1
+      wd = wave_gen.generate_waveform(wave_func, duration_s, freq,
+                                      audio::FrequencyType::CONSTANT, audio::AmplitudeType::CONSTANT, audio::PhaseType::ZERO,
+                                      0.f, sample_rate);
+#else
+      wd = wave_gen.generate_waveform([](float phi, float dur) { return std::sin(phi); }, 1e-2f, freq,
+        audio::FrequencyType::CONSTANT, audio::AmplitudeType::CONSTANT, audio::PhaseType::ZERO, 0.f, 10'000);
+#endif
+      break;
+    case TestType::TEST_FREQ_LAMBDA:
+      wd = wave_gen.generate_waveform(audio::WaveformType::TRIANGLE_WAVE, duration_s, freq,
+                                      freq_func, audio::AmplitudeType::CONSTANT, audio::PhaseType::ZERO, 0.f);
+      break;
+    case TestType::TEST_AMPL_LAMBDA:
+      wd = wave_gen.generate_waveform(audio::WaveformType::SAWTOOTH_WAVE, duration_s, freq,
+                                      audio::FrequencyType::CONSTANT, ampl_func, audio::PhaseType::ZERO, 0.f);
+      break;
+    case TestType::TEST_ALL_LAMBDA:
+      wd = wave_gen.generate_waveform(wave_func, duration_s, freq,
+                                      freq_func, ampl_func, audio::PhaseType::ZERO, 0.f);
+      break;
+    case TestType::TEST_ALL_ENUM:
+      wd = wave_gen.generate_waveform(audio::WaveformType::SAWTOOTH_WAVE, duration_s, freq,
+                                      audio::FrequencyType::JET_ENGINE_POWERUP, audio::AmplitudeType::JET_ENGINE_POWERUP, audio::PhaseType::ZERO, 0.f);
+      break;
+  }
+  
+#if 0
+  auto wd2 = audio.generate_waveform(audio::WaveformType::SINE_WAVE, duration_s, freq*12);
+  auto wd3 = audio.ring_modulation(wd, wd2);
+  auto wd4 = audio::WaveformHelper::resample(wd3, 44'100, audio::LowPassFilterType::Butterworth, 2, 1.2f, 1.f);
+  auto src = src_handler.create_source_from_waveform(wd4);
+  src->play(audio::PlaybackMode::STATE_WAIT);
+#else
+  auto wd2 = audio::WaveformHelper::resample(wd, 44'100, audio::LowPassFilterType::Butterworth, 2, 1.2f, 1.f);
+  auto src = src_handler.create_source_from_waveform(wd2);
+  src->play(audio::PlaybackMode::STATE_WAIT);
+#endif
+  
+  return 0;
+}
+
+
