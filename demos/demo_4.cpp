aa249fc12bf617388e3092775f759258eec84edf
Rasmus Anthin <scratty@gmail.com>
2024-03-17 21:58:58 +0100

    Adding demo 4 : FFT & IFFT.

---
+++ demos/demo_4.cpp
@@ -0,0 +1,37 @@
+//
+//  demo_4.cpp
+//  8Beat
+//
+//  Created by Rasmus Anthin on 2024-03-17.
+//
+
+#include "../AudioSourceHandler.h"
+#include "../WaveformGeneration.h"
+#include "../WaveformIO.h"
+#include "../WaveformHelper.h"
+
+
+
+int main(int argc, char** argv)
+{
+  audio::AudioSourceHandler src_handler;
+  audio::WaveformGeneration wave_gen;
+  
+  std::cout << "FFT:\n";
+  audio::Waveform wd_fft;
+  wd_fft.buffer = { -4, 2, 1, -5 };
+  wd_fft.sample_rate = 10;
+  auto spectrum = audio::WaveformHelper::fft(wd_fft);
+  for (const auto& z : spectrum.buffer)
+    std::cout << z << std::endl;
+  std::cout << spectrum.freq_start << std::endl;
+  std::cout << spectrum.freq_end << std::endl;
+  
+  std::cout << "IFFT:\n";
+  auto wd_ifft = audio::WaveformHelper::ifft(spectrum);
+  for (const auto& s : wd_ifft.buffer)
+    std::cout << s << std::endl;
+  std::cout << wd_ifft.sample_rate << std::endl;
+  
+  return 0;
+}
