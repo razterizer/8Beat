def744686c51907bb53790f5e6ac010347dada9a
Rasmus Anthin <scratty@gmail.com>
2024-05-18 12:36:11 +0200

    Adding stub class SFX and stub enum class SFXType.

---
+++ SFX.h
@@ -0,0 +1,29 @@
+//
+//  SFX.h
+//  8Beat
+//
+//  Created by Rasmus Anthin on 2024-05-18.
+//
+
+#pragma once
+
+#include "WaveformGeneration.h"
+#include "WaveformHelper.h"
+
+
+namespace audio
+{
+  enum class SFXType
+  {
+    COIN,
+    LASER,
+    EXPLOSION,
+    NUM_ITEMS
+  };
+
+  class SFX
+  {
+  public:
+  };
+  
+}
