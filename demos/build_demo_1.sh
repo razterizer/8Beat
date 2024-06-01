e31b79144b56823fa88b20c898fc837374933db6
Rasmus Anthin <scratty@gmail.com>
2024-06-01 17:42:18 +0200

    build_demo_1.sh: * Forgot to remove trailing header search path (duplicate compiler flag).

--- demos/build_demo_1.sh
+++ demos/build_demo_1.sh
@@ -4,8 +4,7 @@ os_name=$(uname)
 
 if [[ $os_name == *"Darwin"* ]]; then
   additional_flags="-I../.. \
-  -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal \
-  -I../.."
+  -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
 else
   additional_flags=""
   export BUILD_PKG_CONFIG_MODULES='openal'
