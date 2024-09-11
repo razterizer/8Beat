9f459e5714caa8c77b62a2d596d4f3c2db496944
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-09-12 00:37:31 +0200

    Update build_demo_3.sh

--- demos/build_demo_3.sh
+++ demos/build_demo_3.sh
@@ -11,3 +11,11 @@ else
 fi
 
 ../../Core/build.sh demo_3 "$1" "${additional_flags[@]}"
+
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
