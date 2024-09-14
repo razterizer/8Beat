349b25b6e0259cd1c750f1261396589050dc164c
Rasmus Anthin <scratty@gmail.com>
2024-09-14 10:28:49 +0200

    Added windows build script for demo 1.

---
+++ demos/build_demo_1.bat
@@ -0,0 +1,9 @@
+call ..\..\Core\build.bat
+ 
+REM Compile the demo using VC++
+cl /std:c++20 /EHsc /Fe:bin/demo_1.exe /Fo:bin/demo_1.obj .\demo_1.cpp /I../..
+
+if %errorlevel% neq 0 (
+    echo Build failed with error code %errorlevel%.
+    exit /b %errorlevel%
+)
