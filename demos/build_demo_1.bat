178fb5d34b8c15376e56369f71edf8fc2e2b7a10
Rasmus Anthin <scratty@gmail.com>
2024-09-17 23:26:03 +0200

    build_demo_1.bat: * Ah. I should "manually" link the program with a separate linker command "link" of course. This hopefully works better.

--- demos/build_demo_1.bat
+++ demos/build_demo_1.bat
@@ -1,7 +1,10 @@
 call ..\..\Core\build.bat
  
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/demo_1.exe /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include /LIBPATH:..\..\..\3rdparty\lib
+cl /std:c++20 /EHsc /Fe:bin/demo_1.obj /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include /I..\..\..\3rdparty\include\OpenAL_Soft
+
+REM Link the compiled object to create the executable
+link /OUT:bin/demo_1.exe bin/demo_1.obj /LIBPATH:..\..\..\3rdparty\lib OpenAL32.lib
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
