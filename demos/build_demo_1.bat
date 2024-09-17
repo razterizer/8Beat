4c08cb149bb8d93123fb0f85dd177863bcf89220
Rasmus Anthin <scratty@gmail.com>
2024-09-17 23:51:09 +0200

    build_demo_1.bat: * Testing.

--- demos/build_demo_1.bat
+++ demos/build_demo_1.bat
@@ -1,7 +1,7 @@
 call ..\..\Core\build.bat
  
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/demo_1.obj /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include
+cl /std:c++20 /EHsc /Fe:bin/demo_1.obj /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include /I..\3rdparty\include
 
 REM Link the compiled object to create the executable
 link /OUT:bin/demo_1.exe bin/demo_1.obj /LIBPATH:..\..\..\3rdparty\lib OpenAL32.lib
