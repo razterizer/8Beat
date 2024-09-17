756f04e9e98796c388056b25b3df50fe1b9fc811
Rasmus Anthin <scratty@gmail.com>
2024-09-17 23:18:54 +0200

    build_demo_1.bat: * Aha, you're supposed to use /LIBPATH instead of /L. Hopefully works better now.

--- demos/build_demo_1.bat
+++ demos/build_demo_1.bat
@@ -1,7 +1,7 @@
 call ..\..\Core\build.bat
  
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/demo_1.exe /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include /L..\..\..\3rdparty\lib
+cl /std:c++20 /EHsc /Fe:bin/demo_1.exe /Fo:bin/demo_1.obj .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include /LIBPATH:..\..\..\3rdparty\lib
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
