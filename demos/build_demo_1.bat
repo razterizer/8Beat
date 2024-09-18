694a78798047b54debf23ed0b32fccaf2eeb9635
Rasmus Anthin <scratty@gmail.com>
2024-09-18 17:16:49 +0200

    build_demo_1.batl: * Testing with stupid answer from ChatGPT.

--- demos/build_demo_1.bat
+++ demos/build_demo_1.bat
@@ -1,8 +1,12 @@
 @echo off
 call ..\..\Core\build.bat
- 
+
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fo:bin/demo_1.obj /Fe:bin/demo_1.exe .\demo_1.cpp /I..\.. /I..\..\..\3rdparty\include
+cl /std:c++20 /EHsc /Fo:bin/demo_1.obj /Fe:bin/demo_1.exe .\demo_1.cpp ^
+/I..\..\Core ^
+/I..\..\Termin8or ^
+/I..\..\AudioLibSwitcher_OpenAL ^
+/I..\..\..\3rdparty\include
 
 if %errorlevel% neq 0 (
     echo Compilation failed with error code %errorlevel%.
