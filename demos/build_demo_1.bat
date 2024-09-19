d939fc75416e5c6936f9a10bcab23cb7caf81a1a
Rasmus Anthin <scratty@gmail.com>
2024-09-19 13:04:38 +0200

    build_demo_1.bat: * Improving the build script demo to use the solution file rather than trying to manually compile and link.

--- demos/build_demo_1.bat
+++ demos/build_demo_1.bat
@@ -1,24 +1,18 @@
 @echo off
 call ..\..\Core\build.bat
 
-REM Compile the demo using VC++
-cl /std:c++20 /EHsc /D_CRT_FUNCTIONS_REQUIRED /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_WARNINGS /Fo:bin/demo_1.obj /Fe:bin/demo_1.obj .\demo_1.cpp ^
-/I..\.. ^
-/I..\..\Core ^
-/I..\..\Termin8or ^
-/I..\..\AudioLibSwitcher_OpenAL ^
-/I..\..\..\3rdparty\include
+cd demos.vs
+
+SET configuration="Release"
+IF "%~1" == "Debug" SET configuration="Debug"
+SET target="x64"
+IF "%~2" == "x86" SET target="x86"
+msbuild demos.sln /p:Configuration=%configuration% /p:Platform=%target%
+
+cd ..
 
 if %errorlevel% neq 0 (
     echo Compilation failed with error code %errorlevel%.
-    exit /b %errorlevel%
-)
-
-REM Link the compiled object to create the executable
-link /OUT:bin/demo_1.exe bin/demo_1.obj /LIBPATH:..\..\..\3rdparty\lib OpenAL32.lib
-
-if %errorlevel% neq 0 (
-    echo Linking failed with error code %errorlevel%.
     exit /b %errorlevel%
 )
 
