c188d0054ead12f0c393b31c73b063ad54b0da82
Rasmus Anthin <scratty@gmail.com>
2024-09-29 12:19:51 +0200

    WaveformHelper_tests.h: * Stub of unit tests of WaveformHelper.h.

--- Tests/WaveformHelper_tests.h
+++ Tests/WaveformHelper_tests.h
@@ -5,8 +5,31 @@
 //  Created by Rasmus Anthin on 2024-09-29.
 //
 
-#ifndef WaveformHelper_Tests_h
-#define WaveformHelper_Tests_h
+#pragma once
+#include "../WaveformHelper.h"
+#include <iostream>
+#include <cassert>
 
+namespace audio
+{
 
-#endif /* WaveformHelper_Tests_h */
+  void unit_tests()
+  {
+    int N = 20;
+    Waveform wave(N, 0.f);
+    wave.frequency = 1.f/(N-1);
+    wave.sample_rate = 1;
+    
+    auto k = math::c_2pi * wave.frequency / wave.sample_rate;
+    for (int i = 0; i < N; ++i)
+      wave.buffer[i] = std::sin(k * i);
+      
+    for (int i = 0; i < N; ++i)
+      std::cout << wave.buffer[i] << std::endl;
+      
+    std::cout << std::endl;
+      
+    WaveformHelper::print_waveform_graph_idx(wave, GraphType::PLOT_THIN, N, 5);
+  }
+
+}
