27193aa5f96971759fe8f4ab36d5d49e979ae98b
Rasmus Anthin <scratty@gmail.com>
2024-12-15 03:38:26 +0100

    setup_and_build_demos_macos.sh: * Making the warning part yellow.

--- setup_and_build_demos_macos.sh
+++ setup_and_build_demos_macos.sh
@@ -13,7 +13,7 @@ REQUIRED_NAME="lib"
 CURRENT_NAME=$(basename "$PWD")
 # Check if the current folder name matches the required name
 if [[ "$CURRENT_NAME" != "$REQUIRED_NAME" ]]; then
-  echo "Warning: You are not in the correct folder. It is highly recommended that you check out the 8Beat repo in a folder named '$REQUIRED_NAME'. The demos will run fine but other repos might expect to find it there."
+  echo -e "${YELLOW}Warning:${RESET} You are not in the correct folder. It is highly recommended that you check out the 8Beat repo in a folder named '$REQUIRED_NAME'. The demos will run fine but other repos might expect to find it there."
   # exit 1
 fi
 
