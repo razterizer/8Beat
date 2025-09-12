83abda46f9e93a46f859bf6cb4fee1d48d55f3b6
Rasmus Anthin <scratty@gmail.com>
2025-09-12 17:20:12 +0200

    Updating bash build scripts with new include path to Termin8or.

--- demos/build_demo_7.sh
+++ demos/build_demo_7.sh
@@ -4,9 +4,11 @@ os_name=$(uname)
 
 if [[ $os_name == *"Darwin"* ]]; then
   additional_flags="-I../.. \
-  -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
+    -I../../Termin8or/include \
+    -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
 else
-  additional_flags="-I../.."
+  additional_flags="-I../.. \
+    -I../../Termin8or/include"
   export BUILD_PKG_CONFIG_MODULES='openal'
 fi
 
