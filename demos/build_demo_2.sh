2b69b5262060aae8d06f723072c8f950dd6e8b4d
Rasmus Anthin <scratty@gmail.com>
2025-09-13 00:58:08 +0200

    Updating include paths in build scripts due to new folder structure in Core repo.

--- demos/build_demo_2.sh
+++ demos/build_demo_2.sh
@@ -4,12 +4,14 @@ os_name=$(uname)
 
 if [[ $os_name == *"Darwin"* ]]; then
   additional_flags="-I../.. \
+    -I../../Core/include \
     -I../../Termin8or/include \
     -I/opt/homebrew/opt/openal-soft/include -I/opt/homebrew/opt/libsndfile/include \
     -L/opt/homebrew/opt/openal-soft/lib -L/opt/homebrew/opt/libsndfile/lib \
     -lopenal -lsndfile"
 else
   additional_flags="-I../.. \
+    -I../../Core/include \
     -I../../Termin8or/include"
   # #FIXME: Add libsndfile here as well.
   export BUILD_PKG_CONFIG_MODULES='openal'
