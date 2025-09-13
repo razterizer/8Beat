7f3483adc07d8260b6ce1c36aa1cede8d41d3caf
Rasmus Anthin <scratty@gmail.com>
2025-09-13 09:53:26 +0200

    Fixing include paths for demo_*.cpp and build_demo_*.sh files.

--- demos/demo_1.cpp
+++ demos/demo_1.cpp
@@ -5,9 +5,9 @@
 //  Created by Rasmus Anthin on 2024-03-16.
 //
 
-#include "../AudioSourceHandler.h"
-#include "../WaveformGeneration.h"
-#include "../WaveformHelper.h"
+#include "AudioSourceHandler.h"
+#include "WaveformGeneration.h"
+#include "WaveformHelper.h"
 #include <Termin8or/input/Keyboard.h>
 
 
