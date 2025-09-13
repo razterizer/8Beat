578b560704bfbbd56b11905ff384ab1d3198b117
Rasmus Anthin <scratty@gmail.com>
2025-09-13 17:36:36 +0200

    Getting rid of unnecessary include of AudioLibSwitcher interface repo.

--- demos/build_demo_4.sh
+++ demos/build_demo_4.sh
@@ -8,7 +8,6 @@ if [[ $os_name == *"Darwin"* ]]; then
     -I../../Core/include \
     -I../../Termin8or/include \
     -I../../TrainOfThought/include \
-    -I../../AudioLibSwitcher/include \
     -I../../AudioLibSwitcher_OpenAL/include \
     -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
 else
@@ -17,7 +16,6 @@ else
     -I../../Core/include \
     -I../../Termin8or/include \
     -I../../TrainOfThought/include \
-    -I../../AudioLibSwitcher/include \
     -I../../AudioLibSwitcher_OpenAL/include"
   export BUILD_PKG_CONFIG_MODULES='openal'
 fi
