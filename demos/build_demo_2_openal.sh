2920c8c6501ab26aef041d5f71391bac882b0daa
Rasmus Anthin <scratty@gmail.com>
2025-09-21 00:41:56 +0200

    Adding OpenAL-based build scripts for demos.

---
+++ demos/build_demo_2_openal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+os_name=$(uname)
+
+if [[ $os_name == *"Darwin"* ]]; then
+  additional_flags="\
+    -I../include/8Beat \
+    -I../../Core/include \
+    -I../../Termin8or/include \
+    -I../../TrainOfThought/include \
+    -I/opt/homebrew/opt/libsndfile/include \
+    -L/opt/homebrew/opt/libsndfile/lib \
+    -I../../AudioLibSwitcher_OpenAL/include \
+    -I/opt/homebrew/opt/openal-soft/include -I/opt/homebrew/opt/libsndfile/include \
+    -L/opt/homebrew/opt/openal-soft/lib -L/opt/homebrew/opt/libsndfile/lib \
+    -lopenal -lsndfile"
+else
+  additional_flags="\
+    -I../include/8Beat \
+    -I../../Core/include \
+    -I../../Termin8or/include \
+    -I../../TrainOfThought/include \
+    -I../../AudioLibSwitcher_OpenAL/include"
+  # #FIXME: Add libsndfile here as well.
+  export BUILD_PKG_CONFIG_MODULES='openal'
+fi
+
+../../Core/build.sh demo_2 "$1" "${additional_flags[@]}"
+
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
---
+++ demos/build_demo_9_openal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+os_name=$(uname)
+
+if [[ $os_name == *"Darwin"* ]]; then
+  additional_flags="\
+    -I../include/8Beat \
+    -I../../Core/include \
+    -I../../Termin8or/include \
+    -I../../TrainOfThought/include \
+    -I../../AudioLibSwitcher_OpenAL/include \
+    -I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
+else
+  additional_flags="\
+    -I../include/8Beat \
+    -I../../Core/include \
+    -I../../Termin8or/include \
+    -I../../TrainOfThought/include \
+    -I../../AudioLibSwitcher_OpenAL/include"
+  export BUILD_PKG_CONFIG_MODULES='openal'
+fi
+
+../../Core/build.sh demo_9 "$1" "${additional_flags[@]}"
+
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
