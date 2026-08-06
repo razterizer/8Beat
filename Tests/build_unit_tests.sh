949e4186fc61a48c97cec32afe0cc97681531596
razterizer <scratty@gmail.com>
2026-08-06 19:54:14 +0200

    Fix audio unit test workflow

--- Tests/build_unit_tests.sh
+++ Tests/build_unit_tests.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 
+os_name=$(uname)
+
 additional_flags="-I../.. \
+  -I../include \
   -I../include/8Beat \
-  -I../../Core/include"
+  -I../../Core/include \
+  -I../../AudioLibSwitcher_applaudio/include \
+  -I../../AudioLibSwitcher_applaudio/AudioLibSwitcher/include \
+  -I../../applaudio/include \
+  -DUSE_APPLAUDIO"
+
+if [[ $os_name == *"Darwin"* ]]; then
+  additional_flags+=" -framework AudioToolbox -framework CoreAudio -framework CoreFoundation"
+else
+  export BUILD_PKG_CONFIG_MODULES='alsa'
+fi
 
 ../../Core/build.sh unit_tests "$1" "${additional_flags[@]}"
 
@@ -14,3 +27,5 @@ if [ $exit_code -ne 0 ]; then
   echo "Core/build.sh failed with exit code $exit_code"
   exit $exit_code
 fi
+
+cp fixtures/async_chain.ct bin/async_chain.ct
